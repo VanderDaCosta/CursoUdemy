37cedbc2f2139d49e3a9eff5b7f38c6f3ff1cfe0
Vander da Costa <opessoaldovander@gmail.com>
2023-07-21 14:23:16 -0300

    atualizações dos estudos até dia 21/07/2023

---
+++ Programacao-OrientadadaObj/aulas/validando formulario/Assets/js/validaCPF.js
@@ -0,0 +1,55 @@
+class ValidaCPF {
+    constructor(cpfEnviado) {
+        Object.defineProperty(this, 'cpfLimpo', {
+            writable: false,
+            enumerable: false,
+            configurable: false,
+            value: cpfEnviado.replace(/\D+/g, ''),
+        });
+    }
+
+    sequencia() {
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+    }
+
+    geraNovoCpf() {
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+        const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+        const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+        this.novoCPF = cpfSemDigitos + digito1 + digito2;
+    }
+
+    static geraDigito(cpfSemDigitos) {
+        let total = 0;
+        let reverso = cpfSemDigitos.length + 1;
+
+        for (let stringNumerica of cpfSemDigitos) {
+            total += reverso * Number(stringNumerica);
+            reverso--;
+        }
+
+        const digito = 11 - (total % 11);
+        return digito <= 9 ? String(digito) : '0';
+    }
+
+    valida() {
+        if (!this.cpfLimpo) return false;
+        if (typeof this.cpfLimpo !== 'string') return false;
+        if (this.cpfLimpo.length !== 11) return false;
+        if (this.sequencia()) return false;
+
+        this.geraNovoCpf();
+
+        return this.novoCPF === this.cpfLimpo;
+
+    }
+}
+
+//let cpf = new ValidaCPf('070.987.720-03');
+//let cpf2 = new ValidaCPf('622.008.673-28');
+//
+//if (cpf.valida()) {
+//    console.log('Cpf válido!');
+//} else {
+//    console.log('Cpf inválido!');
+//}
