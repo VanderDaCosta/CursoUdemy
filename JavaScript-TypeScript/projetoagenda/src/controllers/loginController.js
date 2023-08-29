a1778a7212666f2aa8312fb12655759136d2d3b7
Vander da Costa <opessoaldovander@gmail.com>
2023-08-29 17:01:55 -0300

    att estudos até o dia 29/08/2023

--- JavaScript-TypeScript/projetoagenda/src/controllers/loginController.js
+++ JavaScript-TypeScript/projetoagenda/src/controllers/loginController.js
@@ -1,3 +1,55 @@
+const { async } = require('regenerator-runtime');
+const Login = require('../models/loginModel');
+
 exports.indez = (req, res) => {
     res.render('login');
 };
+
+exports.register = async function (req, res) {//registrar o usuario
+    try {
+        const login = new Login(req.body);
+        await login.register();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+                return res.redirect('back');
+            });
+            return;
+        }
+
+        req.flash('success', 'Seu usuario foi criado com sucesso');
+        req.session.save(function () {
+            return res.redirect('back');
+        });
+
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
+exports.login = async function (req, res) {//logar o usuario
+    try {
+        const login = new Login(req.body);
+        await login.login();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+                return res.redirect('back');
+            });
+            return;
+        }
+
+        req.flash('success', 'Seu usuario foi criado com sucesso');
+        req.session.save(function () {
+            return res.redirect('back');
+        });
+
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
